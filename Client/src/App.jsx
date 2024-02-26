4d8226d28c10b10b9133c221c392d5bd62226fd1
Harikrishna Bomen <harikrishna@creativegalileo.com>
2024-02-26 23:09:55 +0530

    login css completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import HomePage from './Components/HomePage'
+import LoginPage from './Components/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <p>Shridhar</p>
+      {/* <HomePage /> */}
+      <LoginPage />
     </>
   )
 }
