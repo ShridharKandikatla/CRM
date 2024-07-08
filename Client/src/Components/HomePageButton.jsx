7768778f5e9a4044d9e6fd38df2a1c5da3ca84cb
Shridhar Kandikatla <shridharkandikatla@gmail.com>
2024-07-08 20:10:19 +0530

    Update HomePageButton.jsx
    commit

--- Client/src/Components/HomePageButton.jsx
+++ Client/src/Components/HomePageButton.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import useAxios from '../customHooks/useAxios';
-import url from '../../url';
-import { useRecoilState } from 'recoil';
-import { studentAtom } from '../store/atoms/student';
+import React, { useEffect, useState } from "react";
+import useAxios from "../customHooks/useAxios";
+import url from "../../url";
+import { useRecoilState } from "recoil";
+import { studentAtom } from "../store/atoms/student";
 
 const HomePageButton = () => {
   const { loading, error, sendRequest } = useAxios();
@@ -19,103 +19,103 @@ const HomePageButton = () => {
   const BtnName = [
     {
       id: 0,
-      name: 'All',
-      action: () => handleButtonClick('All'),
+      name: "All",
+      action: () => handleButtonClick("All"),
     },
     {
       id: 1,
-      name: 'New',
-      action: () => handleButtonClick('New'),
-      count: getCount('01 - New'),
+      name: "New",
+      action: () => handleButtonClick("New"),
+      count: getCount("01 - New"),
     },
     {
       id: 2,
-      name: 'New (Overdue)',
-      action: () => handleButtonClick('New (Overdue)'),
+      name: "New (Overdue)",
+      action: () => handleButtonClick("New (Overdue)"),
     },
     {
       id: 3,
-      name: 'Call back Followup',
-      action: () => handleButtonClick('Call back Followup'),
-      count: getCount('03-Call Back Followup'),
+      name: "Call back Followup",
+      action: () => handleButtonClick("Call back Followup"),
+      count: getCount("03-Call Back Followup"),
     },
     {
       id: 4,
-      name: 'Call Back (Overdue)',
-      action: () => handleButtonClick('Call Back (Overdue)'),
+      name: "Call Back (Overdue)",
+      action: () => handleButtonClick("Call Back (Overdue)"),
     },
     {
       id: 5,
-      name: 'Interested Followup',
-      action: () => handleButtonClick('Interested Followup'),
-      count: getCount('05-Inetrested Followup'),
+      name: "Interested Followup",
+      action: () => handleButtonClick("Interested Followup"),
+      count: getCount("05-Inetrested Followup"),
     },
     {
       id: 6,
-      name: 'Interested (Overdue)',
-      action: () => handleButtonClick('Interested (Overdue)'),
+      name: "Interested (Overdue)",
+      action: () => handleButtonClick("Interested (Overdue)"),
     },
     {
       id: 7,
-      name: 'Prospect',
-      action: () => handleButtonClick('Prospect'),
-      count: getCount('07-Prospect'),
+      name: "Prospect",
+      action: () => handleButtonClick("Prospect"),
+      count: getCount("07-Prospect"),
     },
     {
       id: 8,
-      name: 'Prospect (Overdue)',
-      action: () => handleButtonClick('Prospect (Overdue)'),
+      name: "Prospect (Overdue)",
+      action: () => handleButtonClick("Prospect (Overdue)"),
     },
     {
       id: 9,
-      name: 'To Be Enrolled',
-      action: () => handleButtonClick('To Be Enrolled'),
-      count: getCount('09-To be enrolled'),
+      name: "To Be Enrolled",
+      action: () => handleButtonClick("To Be Enrolled"),
+      count: getCount("09-To be enrolled"),
     },
     {
       id: 10,
-      name: 'To Be Enrolled - Old',
-      action: () => handleButtonClick('To Be Enrolled - Old'),
+      name: "To Be Enrolled - Old",
+      action: () => handleButtonClick("To Be Enrolled - Old"),
     },
     {
       id: 11,
-      name: 'Recycled',
-      action: () => handleButtonClick('Recycled'),
-      count: getCount('11-Recycled'),
+      name: "Recycled",
+      action: () => handleButtonClick("Recycled"),
+      count: getCount("11-Recycled"),
     },
     {
       id: 12,
-      name: 'Not Interested',
-      action: () => handleButtonClick('Not Interested'),
-      count: getCount('13-Not Interested'),
+      name: "Not Interested",
+      action: () => handleButtonClick("Not Interested"),
+      count: getCount("13-Not Interested"),
     },
     {
       id: 13,
-      name: 'Wrong Mobile Number',
-      action: () => handleButtonClick('Wrong Mobile Number'),
-      count: getCount('15-Wrong Phone Number'),
+      name: "Wrong Mobile Number",
+      action: () => handleButtonClick("Wrong Mobile Number"),
+      count: getCount("15-Wrong Phone Number"),
     },
     {
       id: 14,
-      name: 'Referred From Me',
-      action: () => handleButtonClick('Referred From Me'),
+      name: "Referred From Me",
+      action: () => handleButtonClick("Referred From Me"),
     },
     {
       id: 15,
-      name: 'Re-Equired',
-      action: () => handleButtonClick('Re-Equired'),
+      name: "Re-Equired",
+      action: () => handleButtonClick("Re-Equired"),
     },
   ];
 
   const handleButtonClick = (buttonName) => {
     console.log(buttonName);
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     switch (buttonName) {
-      case 'All':
-        fetch(url + 'student/get', {
-          method: 'POST',
+      case "All":
+        fetch(url + "student/get", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: token,
           },
           body: JSON.stringify({ pageNumber: 1 }),
@@ -123,6 +123,8 @@ const HomePageButton = () => {
           const data = await res.json();
           setStudent(data);
         });
+        break;
+      case "New":
         break;
       default:
         break;
@@ -132,26 +134,26 @@ const HomePageButton = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await sendRequest('get', url + 'student/count');
+        const response = await sendRequest("get", url + "student/count");
         console.log(response);
         setCountData(response);
       } catch (error) {
-        console.error('Error fetching count data:', error);
+        console.error("Error fetching count data:", error);
       }
     };
     fetchData();
   }, []);
 
   return (
-    <div className='pb-3 mx-4'>
+    <div className="pb-3 mx-4">
       {BtnName.map((Menu) => (
         <button
           key={Menu.id}
-          type='button'
-          className='text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-1.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900'
+          type="button"
+          className="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-1.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
           onClick={Menu.action}
         >
-          {Menu.name} {Menu.count ? `(${Menu.count})` : '(0)'}
+          {Menu.name} {Menu.count ? `(${Menu.count})` : "(0)"}
         </button>
       ))}
     </div>
